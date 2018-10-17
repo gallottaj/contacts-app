989b7f7dca35702d75ad94e1ddb67803dc95ad47
Joe Gallotta <joeygallotta@gmail.com>
2018-10-16 19:40:15 -0500

    add create route

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -12,4 +12,12 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find_by(id: contact_id)
     render "show.json.jbuilder"
   end
+
+  def create
+  # I want this to create a new contact
+    @contact = Contact.new(first_name: params[:input_first_name], last_name: params[:input_last_name], email: params[:input_email], phone_number: params[:input_phone_number])
+    render "show.json.jbuilder"
+    @contact.save
+  end
 end
+
